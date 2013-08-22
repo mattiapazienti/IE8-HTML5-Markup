858478a607f7230ae071c64791ba7ba34f73662a
Mattia Pazienti <mattia.pazienti@gmail.com>
2013-08-22 12:41:27 +0200

    First Commit

---
+++ html5-ie8-support.js
@@ -0,0 +1,7 @@
+(function () {
+    if (! /*@cc_on!@*/ 0) return;
+    var e = "abbr,article,aside,audio,bb,canvas,datagrid,datalist,details,dialog,eventsource,figure,figcaption,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video,source".split(',');
+    for (var i = 0; i < e.length; i++) {
+        document.createElement(e[i]);
+    }
+})();
